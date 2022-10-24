4b19b503ff06f1b8a1c603ba1a268d6349b50e4b
Federico Joel Sanchez Nasif <fedesancheznasif@gmailcom>
2022-10-24 19:59:50 -0300

    Primer Pre-Entrega Javascript

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Bienenido al Sistema
+alert("Sistema Control Stock de Mercadería");
+
+// Ingreso Datos Empleado
+let nombreEmpleado = prompt("Ingrese Nombre y Apellido");
+let cuilEmpleado = Number(prompt("Ingrese N° CUIL sin guiones"));
+
+// Bienvenido Usuario
+alert("Bienvenido "+ nombreEmpleado + " N° de Cuil / Registro: " + cuilEmpleado)
+
+// Condicional
+let seleccionProducto = prompt("Ingrese Nombre del Producto a Controlar: Monitor / Teclado / Mouse / Parlante");
+
+while (seleccionProducto != 'Monitor' && seleccionProducto !='Teclado' && seleccionProducto !='Mouse' && seleccionProducto !='Parlante') {
+    alert("El nombre ingresado es incorrecto");
+    seleccionProducto = prompt("Ingrese Nombre del Producto a Controlar: Monitor / Teclado / Mouse / Parlante");
+}
+
+// Control de Flujos
+if (seleccionProducto == "Monitor") {
+    alert("Ud. realizará el control de Stock de Monitores")
+} else if (seleccionProducto == "Teclado") {
+    alert("Ud. realizará el control de Stock de Teclados")
+} else if (seleccionProducto == "Mouse") {
+    alert("Ud. realizará el control de Stock de Mouses")
+} else {
+    alert("Ud. realizará el control de Stock de Parlantes")
+}
