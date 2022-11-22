1e8cbe1684060e774c727a012ffe6b0e3eacb284
Federico Joel Sanchez Nasif <fedesancheznasif@gmailcom>
2022-11-21 22:42:24 -0300

    3er Pre Entrega JS

--- js/main.js
+++ js/main.js
@@ -1,31 +1,50 @@
-// Bienenido al Sistema
-alert("Sistema Control Stock de Mercadería");
+// Bienvenido
+alert ("Bienvenido al sistema de cotización de productos");
 
-// Ingreso Datos Empleado
-let nombreEmpleado = prompt("Ingrese Nombre y Apellido");
-let cuilEmpleado = Number(prompt("Ingrese N° CUIL sin guiones"));
+// Ingreso de Datos
+let nombre = prompt("Ingrese Nombre y Apellido");
 
-// Bienvenido Usuario
-function saludar(nombreEmpleado, cuilEmpleado) {
-    alert("Bienvenido "+ nombreEmpleado + " N° de Cuil / Registro: " + cuilEmpleado)
+// Funcion
+function saludar (nombre) {
+    alert ("Bienvenido " + nombre);
 }
-saludar(nombreEmpleado, cuilEmpleado);
+saludar(nombre);
 
-// Condicional
-let seleccionProducto = prompt("Ingrese Nombre del Producto a Controlar: Monitor / Teclado / Mouse / Parlante");
+// Control de Flujos y Condicional
+let producto = prompt("Que producto desea cotizar? Remera / Jean / Buzo");
 
-while (seleccionProducto != 'Monitor' && seleccionProducto !='Teclado' && seleccionProducto !='Mouse' && seleccionProducto !='Parlante') {
-    alert("El nombre ingresado es incorrecto");
-    seleccionProducto = prompt("Ingrese Nombre del Producto a Controlar: Monitor / Teclado / Mouse / Parlante");
+while (producto != "Remera" && producto != "Jean" && producto != "Buzo") {
+    alert ("Producto Incorrecto");
+    producto = prompt("Que producto desea cotizar? Remera / Jean / Buzo")
 }
 
-// Control de Flujos
-if (seleccionProducto == "Monitor") {
-    alert("Ud. realizará el control de Stock de Monitores")
-} else if (seleccionProducto == "Teclado") {
-    alert("Ud. realizará el control de Stock de Teclados")
-} else if (seleccionProducto == "Mouse") {
-    alert("Ud. realizará el control de Stock de Mouses")
+if (producto == "Remera") {
+    alert("Producto: Remera. Valor de contado: $4990");
+} else if (producto == "Jean") {
+    alert("Producto: Jean. Valor de contado: $9990");
 } else {
-    alert("Ud. realizará el control de Stock de Parlantes")
+    alert("Producto: Buzo. Valor de contado: $8990");  
+}
+
+// Objetos
+class Producto {
+    constructor (nombre, precio) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+    }
+    precioEnCuotas() {
+        this.precio = this.precio * 1.2;
+        return this.precio;
+    }
+}
+
+// Array
+const productos = [];
+
+productos.push (new Producto ("Remera", "4990"));
+productos.push (new Producto ("Jean", "9990"));
+productos.push (new Producto ("Buzo", "8990"));
+
+for (const producto of productos) {
+    alert("Producto: " + producto.nombre + "; Precio de contado: $" + producto.precio + "; Precio en Cuotas: $" + producto.precioEnCuotas());
 }
