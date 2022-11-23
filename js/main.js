e116450b2538a8dee986487ab5e96dd9459145e9
Federico Joel Sanchez Nasif <fedesancheznasif@gmailcom>
2022-11-23 20:21:59 -0300

    Modificaciones entrega

--- js/main.js
+++ js/main.js
@@ -11,19 +11,21 @@ function saludar (nombre) {
 saludar(nombre);
 
 // Control de Flujos y Condicional
-let producto = prompt("Que producto desea cotizar? Remera / Jean / Buzo");
+let producto = prompt("Que producto desea cotizar? Remera / Jean / Camisa / Buzo");
 
-while (producto != "Remera" && producto != "Jean" && producto != "Buzo") {
+while (producto != "Remera" && producto != "Jean" && producto != "Camisa" && producto != "Buzo") {
     alert ("Producto Incorrecto");
-    producto = prompt("Que producto desea cotizar? Remera / Jean / Buzo")
+    producto = prompt("Que producto desea cotizar? Remera / Jean / Camisa / Buzo")
 }
 
 if (producto == "Remera") {
-    alert("Producto: Remera. Valor de contado: $4990");
+    alert("Producto: Remera. Valor de contado: $4.990");
 } else if (producto == "Jean") {
-    alert("Producto: Jean. Valor de contado: $9990");
+    alert("Producto: Jean. Valor de contado: $9.990");
+} else if (producto == "Camisa") {
+    alert("Producto: Camisa. Valor de contado: $7.530");
 } else {
-    alert("Producto: Buzo. Valor de contado: $8990");  
+    alert("Producto: Buzo. Valor de contado: $8.990");  
 }
 
 // Objetos
@@ -43,8 +45,34 @@ const productos = [];
 
 productos.push (new Producto ("Remera", "4990"));
 productos.push (new Producto ("Jean", "9990"));
+productos.push (new Producto ("Camisa", "7530"));
 productos.push (new Producto ("Buzo", "8990"));
 
+
 for (const producto of productos) {
-    alert("Producto: " + producto.nombre + "; Precio de contado: $" + producto.precio + "; Precio en Cuotas: $" + producto.precioEnCuotas());
+    alert("Producto: " + producto.nombre + "; Precio de contado: $" + producto.precio + "; Precio en 3 Cuotas: $" + producto.precioEnCuotas());
 }
+
+// Funciones de Orden Superior
+const seisCuotas = productos.map((seis) => Math.round(seis.precio * 1.4));
+let masCuotas = prompt('Desea saber el precio en 6 cuotas? Si / No')
+
+while (masCuotas != "Si" && masCuotas != "No") {
+    alert('Dato ingresado Incorrecto')
+    masCuotas = prompt('Desea saber el precio en 6 cuotas? Si / No')
+}
+
+if (masCuotas === 'Si') {
+    alert('Remera en 6 cuotas: $'+ seisCuotas[0] + ', Jean en 6 cuotas: $' + seisCuotas[1] + ', Camisa en 6 cuotas: $' + seisCuotas[2] + ', Buzo en 6 cuotas: $' + seisCuotas[3])
+} else {
+    alert ('Gracias igualmente por su consulta')
+}
+
+
+
+
+
+
+
+
+
